dd81ef7003fd124ec6330ab0ccad86b3664249c7
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2023-05-18 19:25:06 +0530

    Time: 97 ms (97.00%), Space: 61.2 MB (60.92%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int ans;
-        int l= nums.size();
-        sort(nums.begin(),nums.end());
-        int i=0;
-       while(i<l)
-        {
-            // if(count(nums.begin(),nums.end(),i)>1)
-            if(nums[i]==nums[i+1])
-            {
-                ans=nums[i];
-                break;
-            }
-           i++;
-        }
-        return ans;
+      //using the floyd hare and turtoise algo
+
+      int fast=0,slow=0,slower=0;
+
+      while(true){
+          slow=nums[slow];
+          fast=nums[nums[fast]];//going at twice of speed
+if(fast==slow)//intersetion point
+break;
+      }
+
+      while(true){
+slower=nums[slower];
+slow=nums[slow];
+if(slow==slower)
+break;
+      }
+      return slow;
+      
+        // int ans;
+        // int l= nums.size();
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<l;i++)
+        // {
+        //     // if(count(nums.begin(),nums.end(),i)>1)
+        //     if(nums[i]==nums[i+1])
+        //     {
+        //         ans=nums[i];
+        //         break;
+        //     }
+        // }
+        // return ans;
     }
 };
