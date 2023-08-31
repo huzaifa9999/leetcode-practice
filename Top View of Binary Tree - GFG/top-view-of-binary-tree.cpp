620c07e907db9f1c64668bd71a002d6dc0bc3bf9
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2023-08-31 11:58:19 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -104,41 +104,32 @@ class Solution
     //from left to right in Binary Tree.
     vector<int> topView(Node *root)
     {
-        //Your code here
-          vector<int>ans;
-          if(root==NULL) return ans;
-        queue<pair<Node*,int>> q;
+        vector<int>ans;
+        queue<pair<Node*,int>>q;
         map<int,int>mp;
+        if(root==NULL) return ans;
+        // int line =0;
         q.push({root,0});
-      
-      
-        
         while(!q.empty())
         {
+            
             auto it=q.front();
             q.pop();
-            Node* node=it.first;
-            int line=it.second;
-            if(mp.find(line)==mp.end())
-            mp[line]=node->data;
-            
-            if(node->left!=NULL){
-                q.push({node->left,line-1});
-                
-            }
-            
-            if(node->right!=NULL)
-            {
-                q.push({node->right,line+1});
-            }
+           Node* node=it.first;
+        int line =it.second;
+
+             if(mp.find(line)==mp.end()) mp[line]=node->data;
+             if(node->left!=NULL) q.push({node->left,line-1});
+             if(node->right!=NULL) q.push({node->right,line+1});
+             
         }
         
         for(auto it:mp)
         {
             ans.push_back(it.second);
         }
-    
-        return ans;
+       
+        //Your code here
     }
 
 };
