4ad88e29bf203b3617eee22d36d0d26ee77d6da2
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2023-09-13 19:53:19 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -7,34 +7,53 @@ class Solution {
   public:
     // Function to detect cycle in an undirected graph.
     
-    bool check(int root, vector<int>adj[],int vis[])
-    {
-         vis[root]=1;
+    
+    //BFS
+    // bool check(int root, vector<int>adj[],int vis[])
+    // {
+    //      vis[root]=1;
         
-        queue<pair<int,int>>q;
+    //     queue<pair<int,int>>q;
         
-        q.push({root,-1});
+    //     q.push({root,-1});
         
        
         
-        while(!q.empty())
-        {
-            int node= q.front().first;
-            int level= q.front().second;
-            q.pop();
-            for(auto it: adj[node])
-            {
-                if(!vis[it])
-                { vis[it]=1;
-                    q.push({it,node});
-                }
-               else if(level!=it) return true;
+    //     while(!q.empty())
+    //     {
+    //         int node= q.front().first;
+    //         int level= q.front().second;
+    //         q.pop();
+    //         for(auto it: adj[node])
+    //         {
+    //             if(!vis[it])
+    //             { vis[it]=1;
+    //                 q.push({it,node});
+    //             }
+    //           else if(level!=it) return true;
                 
+    //         }
+    //     }
+        
+    // return false;
+        
+    // }
+    
+    
+    bool dfs(int root,int level,vector<int>adj[],int vis[])
+    {
+        vis[root]=1;
+        
+        for(auto it : adj[root])
+        {
+            if(!vis[it])
+            {
+                if(dfs(it,root,adj,vis)==true) return true;
             }
+            else if(it!=level) return true;
         }
         
-    return false;
-        
+        return false;
     }
     
     bool isCycle(int V, vector<int> adj[]) {
@@ -45,7 +64,7 @@ class Solution {
     {
         if(!vis[i])
         {
-            if(check(i,adj,vis)==true) return true;
+            if(dfs(i,-1,adj,vis)==true) return true;
         }
     }
         return false;
