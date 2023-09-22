35ab31d48358f7f2572add54db8239f9590ecd2c
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2023-09-22 18:51:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -7,41 +7,71 @@ class Solution {
   public:
     // Function to detect cycle in a directed graph.
     
-     bool check(int root, vector<int>adj[],int vis[],int pvis[])
+    //  bool check(int root, vector<int>adj[],int vis[],int pvis[])
      
-    {
-     vis[root]=1;
-     pvis[root]=1;
+    // {
+    //  vis[root]=1;
+    //  pvis[root]=1;
      
-     for(auto it: adj[root])
-     {
-         if(!vis[it])
-         {
-             if(check(it,adj,vis,pvis)==true) return true;
-         }
-         else if(pvis[it]==1) return true;
-     }
+    //  for(auto it: adj[root])
+    //  {
+    //      if(!vis[it])
+    //      {
+    //          if(check(it,adj,vis,pvis)==true) return true;
+    //      }
+    //      else if(pvis[it]==1) return true;
+    //  }
      
-     pvis[root]=0;
-        return false;
-    }
+    //  pvis[root]=0;
+    //     return false;
+    // }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
     
         
-        int vis[V]={0};
-        int pvis[V]={0};
+    //     int vis[V]={0};
+    //     int pvis[V]={0};
         
-    for(int i=0;i<V;i++)
-    {
-        if(!vis[i])
-        {
-            if(check(i,adj,vis,pvis)==true) return true;
-        }
+    // for(int i=0;i<V;i++)
+    // {
+    //     if(!vis[i])
+    //     {
+    //         if(check(i,adj,vis,pvis)==true) return true;
+    //     }
     
          
+    // }
+    //   return false;
+    
+    vector<int>topo;
+    queue<int>q;
+    int indegree[V]={0};
+    for(int i=0;i<V;i++)
+    {
+        for(auto it: adj[i])
+        {
+            indegree[it]++;
+        }
     }
-       return false;
+    
+    for(int i=0;i<V;i++)
+    {
+        if(indegree[i]==0) q.push(i);
+    }
+    
+    while(!q.empty())
+    {
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it: adj[node])
+        {
+            indegree[it]--;
+            if(indegree[it]==0) q.push(it);        }
+    }
+    
+    if(topo.size()!=V) return true;
+    return false;
     }
 };
 
