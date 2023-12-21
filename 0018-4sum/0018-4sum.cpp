010c7462e5cf20e2241bd4f64ad6019385e243ab
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2023-12-21 13:44:29 +0530

    Time: 116 ms (29.82%), Space: 13.6 MB (79.21%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(i>0&&nums[i]==nums[i-1])continue;
+            if(i>0&&nums[i]==nums[i-1]) continue;
             
-            else
+            for(int j=i+1;j<nums.size();j++)
             {
-                for(int j=i+1;j<n;j++){
-                    if(j>i+1&&nums[j]==nums[j-1])continue;
-                else
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                
+                int l=j+1,h=nums.size()-1;
+                while(l<h)
                 {
-                    int l=j+1;
-                    int h=n-1;
-                    while(l<h)
-                    {
-                        long long  sum = (long long)nums[i]+(long long)(nums[j])+(long long)(nums[l])+(long long)(nums[h]);
-                        if(sum==(long long) (target))
-                        {
-                            ans.push_back({nums[i],nums[j],nums[l],nums[h]});
-                            l++;h--;
-                            while(l<h&&nums[h]==nums[h+1])h--;
-                            while(l<h&&nums[l]==nums[l-1])l++;
-                            
-                        }
-                        
-                        else if(sum>(long long) (target))h--;
-                        else l++;
-                    }
-                }
+                if(((long long)nums[i]+(long long)nums[j]+(long long)nums[l]+(long long)nums[h])==(long long)(target))
+                {ans.push_back({nums[i],nums[j],nums[l],nums[h]});
+                    l++;
+                    h--;
+                    
+                    while(l<h&&nums[h]==nums[h+1]) h--;
+                    while(l<h&&nums[l]==nums[l-1]) l++;}
+                    
+                    else if(((long long)nums[i]+(long long)nums[j]+(long long)nums[l]+(long long)nums[h])>(long long )(target) ) h--;
+                    else
+                        l++;
+            
+                    
                 }
             }
         }
