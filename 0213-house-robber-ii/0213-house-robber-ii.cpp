ea86678718c781207d32061ae8cf2a7b6d9b5851
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-03 12:53:07 +0530

    Time: 2 ms (43.09%), Space: 8.1 MB (86.86%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
-public:    
-        int func(vector<int>&nums,int n)
-    { vector<int>dp(n,-1);
-       dp[0]=nums[0]; 
-        // if(dp[n]!=-1) return dp[n];
+public: 
+    
+     int func(vector<int>&nums,int n)
+    {
+       int prev=nums[0]; 
+ int prev2;
         for(int i=1;i<n;i++)
         {
         int pick= nums[i];
             
-            if(i>1) pick+=dp[i-2];
-        int notpick=dp[i-1];
-        dp[i]=max(pick,notpick);
+            if(i>1) pick+=prev2;
+        int notpick=prev;
+        int curi=max(pick,notpick);
+            prev2=prev;
+            prev=curi;
         }
-        return dp[n-1];
+        return prev;
     }
     
     int rob(vector<int>& nums) {
@@ -27,10 +30,43 @@ public:
         
         int ans1=func(first,first.size());
         int ans2=func(second,second.size());
-        return max(ans1,ans2);
-        
+        return max(ans1,ans2);        
         
     }
+    
+    
+    //tabulation
+//         int func(vector<int>&nums,int n)
+//     { vector<int>dp(n,-1);
+//        dp[0]=nums[0]; 
+ 
+//         for(int i=1;i<n;i++)
+//         {
+//         int pick= nums[i];
+            
+//             if(i>1) pick+=dp[i-2];
+//         int notpick=dp[i-1];
+//         dp[i]=max(pick,notpick);
+//         }
+//         return dp[n-1];
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>first,second;
+//         if(n==1) return nums[0];
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(i!=0) first.push_back(nums[i]);
+//             if(i!=nums.size()-1) second.push_back(nums[i]);
+//         }
+        
+//         int ans1=func(first,first.size());
+//         int ans2=func(second,second.size());
+//         return max(ans1,ans2);
+        
+        
+//     }
     
     
     
