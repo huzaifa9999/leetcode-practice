4279fd1dc3839adf516fa00a034ada16c5541663
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-03 12:24:16 +0530

    Time: 8 ms (24.95%), Space: 8.2 MB (38.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,41 +2,20 @@ class Solution {
 public:
     int func(vector<int>& nums,int n,vector<int>&dp)
     {
-        if(n<0) return 0;
-        if(n==0) return nums[0];
+   if(n<0) return 0;
+        if(n ==0) nums[0];
         if(dp[n]!=-1) return dp[n];
-       int pick=nums[n]+func(nums,n-2,dp);
-       int notpick=0+func(nums,n-1,dp);
+        int pick= nums[n]+ func(nums,n-2,dp);
+            int notpick=func(nums,n-1,dp);
+        
         return dp[n]=max(pick,notpick);
+        
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        // int ans=func(nums,n-1,dp);
-//         dp[0]=nums[0];
-//         for(int i=1;i<n;i++)
-//         { 
-//             int pick=nums[i];
-//             if(i>1) pick+=dp[i-2];
-//         int notpick=dp[i-1];
-            
-//             dp[i]=max(pick,notpick);
-//         }
-//         return dp[n-1];
-        int prev=nums[0],prev2;
-        
-        for(int i=1;i<n;i++)
-        {
-            int pick=nums[i];
-            if(i>1) pick+=prev2;
-            int notpick=prev;
-             int curi= max(pick,notpick);
-            
-            prev2=prev;
-            prev=curi;
-            
-        }
-        return prev;
+        vector<int>dp(n+1,-1);
+  int ans=func(nums,n-1,dp);
+        return ans;
         
     }
 };
