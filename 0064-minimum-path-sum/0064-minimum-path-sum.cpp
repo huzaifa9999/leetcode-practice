f05eec3a9fe1eff9ea349ab0a187eeb49bff7234
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-04 20:03:47 +0530

    Time: 2 ms (97.96%), Space: 10.2 MB (70.27%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int f(int m,int n, vector<vector<int>>& grid,vector<vector<int>>& dp)
-    {
-        if(m==0&&n==0) return grid[0][0];
-        if(m<0||n<0) return 1e9;
-        if(dp[m][n]!=-1)  return dp[m][n];
-        int left= grid[m][n]+f(m,n-1,grid,dp);
-            int up=grid[m][n]+f(m-1,n,grid,dp);
+//     int f(int m,int n, vector<vector<int>>& grid,vector<vector<int>>& dp)
+//     {
+//         if(m==0&&n==0) return grid[0][0];
+//         if(m<0||n<0) return 1e9;
+//         if(dp[m][n]!=-1)  return dp[m][n];
+//         int left= grid[m][n]+f(m,n-1,grid,dp);
+//             int up=grid[m][n]+f(m-1,n,grid,dp);
         
-        return dp[m][n]=min(up,left);
-    }
+//         return dp[m][n]=min(up,left);
+//     }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+     vector<int>prev(n,0);
         
         for(int i=0;i<m;i++)
-        {
+        { vector<int>curi(n,0);
             for(int j=0;j<n;j++)
             {
                 if(i==0&&j==0) 
-                {dp[i][j]= grid[0][0];continue;}
+                {curi[j]= grid[0][0];continue;}
                 int left=0,up=0;
-              up=grid[i][j] ; if(i>0) up+=dp[i-1][j]; else up+=1e9;
-            left=grid[i][j]  ;  if(j>0) left+=dp[i][j-1]; else left+=1e9;
+              up=grid[i][j] ; if(i>0) up+=prev[j]; else up+=1e9;
+            left=grid[i][j]  ;  if(j>0) left+=curi[j-1]; else left+=1e9;
                 
                 
-                dp[i][j]=min(left,up);
+                curi[j]=min(left,up);
             }
+         prev=curi;
         }
         // int ans= f(m-1,n-1,grid,dp);
-    return dp[m-1][n-1];
+    return prev[n-1];
     }
 };
