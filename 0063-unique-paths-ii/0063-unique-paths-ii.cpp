0425d3b8ea7417b469c2b5d4cbe5b716b5c61e6d
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-04 19:38:27 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (23.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -17,8 +17,24 @@ public:
         int n= obstacleGrid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
         
-        int ans=f(m-1,n-1,dp,obstacleGrid);
-        return ans;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            { 
+                int right=0,left=0;
+                if(m>0&&n>0&&obstacleGrid[i][j]==1) dp[i][j]=0;
+               else  if(i==0&&j==0) dp[0][0]=1;
+               else{
+                   if(j>0) left=dp[i][j-1];
+                 if (i>0) right=dp[i-1][j];
+             
+                dp[i][j]=left+right;
+                   }
+            }
+        }
+        // int ans=f(m-1,n-1,dp,obstacleGrid);
+        return dp[m-1][n-1];
         
     }
 };
