c171520934ea29cea976a5cce0e86c5f3f662fb0
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-06 11:46:49 +0530

    Time: 8 ms (88.79%), Space: 13.8 MB (79.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
+     
+        int m= matrix.size();
         int n=matrix[0].size();
-        vector<pair<int,int>> v;
+        
+        vector<pair<int,int>> cord;
         
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(matrix[i][j]==0)
-                {
-                    v.push_back({i,j});
-                }
+                    cord.push_back({i,j});
             }
         }
-        
-        for(int i=0;i<v.size();i++)
-        {
-            int x=v[i].first;
-            int y=v[i].second;
-            
-            for(int i=0;i<n;i++)
-            {
-                matrix[x][i]=0;
-            }
-            
-             for(int i=0;i<m;i++)
+        for(auto it:cord)
+        { int x=it.first;
+         int y=it.second;
+         
+         
+            for(int i=0;i<m;i++)
             {
                 matrix[i][y]=0;
+            }
+          for(int i=0;i<n;i++)
+            {
+                matrix[x][i]=0;
             }
         }
         
