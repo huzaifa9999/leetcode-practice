2fdd5c6682aa8e736f2b167cc2d360b5789d7170
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-10 19:37:10 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (54.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,63 +1,49 @@
-class Solution
-{
+class Solution {
 public:
-    int orangesRotting(vector<vector<int>> &grid)
-    {
-
-        queue<pair<pair<int, int>, int>> q;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n,0));
-
-        int fresh = 0;
-        for (int i = 0; i < grid.size(); i++)
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        queue<pair<pair<int,int>,int>> q;
+        int m=grid.size();int n=grid[0].size();
+        int fresh=0;
+        for(int i=0;i<grid.size();i++)
         {
-            for (int j = 0; j < grid[0].size(); j++)
-            {
-                if (grid[i][j] == 2)
-                {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                }
-                if (grid[i][j] == 1)
-                {
-                    fresh++;
-                }
-                
+            for(int j=0;j<grid[0].size();j++)
+            { if(grid[i][j]==2)
+                q.push({{i,j},0});
+             
+             if(grid[i][j]==1) fresh++;
             }
         }
-
-        int dx[] = {1, 0, -1, 0};
-        int dy[] = {0, 1, 0, -1};
-
-        int ans = 0;
-        int count = 0;
-        while (!q.empty())
+        
+        int dx[]={1,0,-1,0};
+        int dy[]={0,1,0,-1};
+        
+        int c=0;
+        while(!q.empty())
         {
-            int x = q.front().first.first;
-            int y = q.front().first.second;
-            int starttime = q.front().second;
-            ans = max(ans, starttime);
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int time=q.front().second;
+            c=max(c,time);
             q.pop();
-
-            for (int i = 0; i < 4; i++)
+            
+            for(int i=0;i<4;i++)
             {
-                int newx = x + dx[i];
-                int newy =y +dy[i];
-
-                if (newx >= 0 && newy >= 0 && newx < m && newy < n && grid[newx][newy] == 1 && vis[newx][newy] != 2)
-                {
-                    count++;
-                    vis[newx][newy] = 2;
-
-                    q.push({{newx, newy}, starttime + 1});
-                }
+                int newx=x+dx[i];
+                    int newy=y+dy[i];
+            
+            if(newx>=0&&newy>=0&&newx<m&&newy<n&&grid[newx][newy]==1)
+            { fresh--;
+                grid[newx][newy]=2;
+                q.push({{newx,newy},time+1});
             }
+            }
+            // c++;
+            
         }
-
-        if (fresh == count)
-            return ans;
-        else
-            return -1;
+        
+        if(fresh==0) return c;
+        else return -1;
+        
     }
 };
