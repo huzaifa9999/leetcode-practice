1ae9f22cb35dc82ef90e52515adda1398021a647
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-13 13:41:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -8,29 +8,28 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        vector<int>ans;
         
-         int vis[V]={0};
-         vis[0]=1;
-         
-         queue<int>q;
-         
-         q.push(0);
-         while(!q.empty())
-         {
-             int node= q.front();
-             q.pop();
-             ans.push_back(node);
-             for(auto it:adj[node])
-             {
-                 if(vis[it]!=1)
-                 {
-                     vis[it]=1;
-                     q.push(it);
-                 }
-             }
-         }
-         return ans;
+        vector<int>vis(V,0);
+        queue<int>q;
+    vector<int>ans;    
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            int ele=q.front();
+            q.pop();
+          
+            ans.push_back(ele);
+            for(auto &it:adj[ele])
+            { if(vis[it]==0)
+            {vis[it]=1;
+                q.push(it);
+                
+            }
+            }
+        }
+        
+        return ans;
         
     }
 };
