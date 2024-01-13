251ec1d683836ca363b4f349a467a67fb7756c6d
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-13 15:08:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -8,67 +8,39 @@ class Solution {
     // Function to detect cycle in an undirected graph.
     
     
-    //BFS
-    // bool check(int root, vector<int>adj[],int vis[])
-    // {
-    //      vis[root]=1;
-        
-    //     queue<pair<int,int>>q;
-        
-    //     q.push({root,-1});
-        
-       
-        
-    //     while(!q.empty())
-    //     {
-    //         int node= q.front().first;
-    //         int level= q.front().second;
-    //         q.pop();
-    //         for(auto it: adj[node])
-    //         {
-    //             if(!vis[it])
-    //             { vis[it]=1;
-    //                 q.push({it,node});
-    //             }
-    //           else if(level!=it) return true;
-                
-    //         }
-    //     }
-        
-    // return false;
-        
-    // }
-    
-    
-    bool dfs(int root,int level,vector<int>adj[],int vis[])
+    bool check(int root , vector<int>adj[],vector<int>&vis)
     {
-        vis[root]=1;
         
-        for(auto it : adj[root])
-        {
-            if(!vis[it])
-            {
-                if(dfs(it,root,adj,vis)==true) return true;
-            }
-            else if(it!=level) return true;
+          queue<pair<int, int>> q; // node, parent;
+
+ 
+    q.push({root, -1});  // Start from node 1
+    vis[root] = 1;
+
+    while (!q.empty()) {
+        int ele = q.front().first;
+        int parent = q.front().second;
+        q.pop();
+        for (auto it : adj[ele]) {
+            if (!vis[it]) {
+                vis[it] = 1;
+                q.push({it, ele});
+            } else if (parent != it)
+                return true;
         }
-        
-        return false;
     }
-    
+    return false;
+    }
     bool isCycle(int V, vector<int> adj[]) {
-        
-        int vis[V]={0};
-        
-    for(int i=0;i<V;i++)
-    {
-        if(!vis[i])
-        {
-            if(dfs(i,-1,adj,vis)==true) return true;
-        }
-    }
-        return false;
         // Code here
+        vector<int>vis(V,0);
+        
+        for(int i=0;i<V;i++)
+        { if(!vis[i])
+            if(check(i,adj,vis)==true) return true;
+        }
+        return false;
+
     }
 };
 
