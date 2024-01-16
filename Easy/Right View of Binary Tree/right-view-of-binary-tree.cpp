43ddb7c50ce0fb713b304938e8b5ece0e4e15cf0
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-01-16 12:46:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
+++ Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -39,21 +39,28 @@ struct Node
 // Should return  right view of tree
 class Solution
 {
+    
+    
     public:
+    void check(Node *root, vector<int>&ans,int level)
+
+{
+if(root==NULL) return ;
+
+if(ans.size()==level) ans.push_back(root->data);
+
+check(root->right,ans,level+1);
+check(root->left,ans,level+1);
+
+}
     //Function to return list containing elements of right view of binary tree.
-    void right(Node* root,vector<int>&ans,int level)
-    {
-        if(root==NULL) return ;
-        if(level==ans.size()) ans.push_back(root->data);
-        right(root->right,ans,level+1);
-         right(root->left,ans,level+1);
-    }
     vector<int> rightView(Node *root)
     {
        // Your Code here
-       vector<int>ans;
-       right(root,ans,0);
-       return ans;
+        vector<int>ans;
+
+        check(root,ans,0);
+return ans;
     }
 };
 
