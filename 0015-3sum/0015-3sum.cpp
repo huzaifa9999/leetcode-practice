523dab1dcf04ce295739104f88e16fc182718ddf
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-06-23 13:02:39 +0530

    Time: 2019 ms (5.00%), Space: 474.1 MB (5.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;int index=0;
-        set<vector<int>>set;
+      vector<vector<int>>ans;
+        set<vector<int>>s;
+        int index=0;
+            sort(nums.begin(),nums.end());
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++)
-        {
-            
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            
-            unordered_map<int,int>mp;
-            
-            for(int j=i+1;j<nums.size();j++)
-            { int target=nums[i]+nums[j];
-                if(mp.find(-(target))!=mp.end())
-                {
-                    set.insert({nums[i],nums[j],-(target)});
-                
-                }
-              mp[nums[j]]=j;
-            }
+        { index=nums[i];
+          if(i>0&&nums[i]==nums[i-1]) continue;
+         unordered_map<int,int>mp;
+         for(int j=i+1;j<nums.size();j++)
+         {
+             int target=-(nums[i]+nums[j]);
+             
+             if(mp.find(target)!=mp.end())
+             {
+                 vector<int>temp={nums[i],nums[j],-(nums[i]+nums[j])};
+                 // sort(temp.begin(),temp.end());
+                 s.insert(temp);
+             }
+             mp[nums[j]]=j;
+         }
+
         }
-        // sort(ans.begin(),ans.end());
-        for(auto it:set)
+        
+        for(auto it:s)
         {
             ans.push_back(it);
         }
