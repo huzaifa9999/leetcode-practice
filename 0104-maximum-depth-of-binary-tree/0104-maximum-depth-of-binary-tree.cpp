c2af7b09becb19432622c030507b8200f0880b46
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-07-07 13:07:04 +0530

    Time: 5 ms (62.99%), Space: 17.5 MB (45.42%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,22 +11,31 @@
  */
 class Solution {
 public:
+   int helper(TreeNode* root,int level)
+    {
+        if(root==NULL) return 0;
+        int left=helper(root->left,level);
+        int right=helper(root->right,level);
+        level= max(left,right);
+        return 1+max(left,right);
+    }
     int maxDepth(TreeNode* root) {
         if(root==NULL) return 0;
-        queue<pair<TreeNode*,int>>q;
+//         queue<pair<TreeNode*,int>>q;
         
-        int ans=1;
-        q.push({root,1});
-        while(!q.empty())
-        {
-        TreeNode* node=q.front().first;
-            int level=q.front().second;
-            q.pop();
-            if(node->left) q.push({node->left,level+1});
-            if(node->right) q.push({node->right,level+1});
-            ans=max(ans,level);
-        }
+//         int ans=1;
+//         q.push({root,1});
+//         while(!q.empty())
+//         {
+//         TreeNode* node=q.front().first;
+//             int level=q.front().second;
+//             q.pop();
+//             if(node->left) q.push({node->left,level+1});
+//             if(node->right) q.push({node->right,level+1});
+//             ans=max(ans,level);
+//         }
         
-        return ans;
+//         return ans;
+     return helper(root,0);
     }
 };
