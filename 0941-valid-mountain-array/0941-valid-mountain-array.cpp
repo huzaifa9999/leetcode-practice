33328e2bec44d8e32908954ce1720ba6c3075bc3
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2024-12-17 11:39:58 +0530

    Time: 5 ms (99.70%), Space: 26.3 MB (13.15%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,43 +1,57 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-         int n=arr.size();
-        if(n==1)
-        return false;
-
-//now checking for rest of the cases which could return false
-        int flag=1;
-        for(int i=1;i<n;i++){
-            //if increasing change to 0 i.e, if decreasing array is there or not
-            if(arr[i]<arr[i-1])
-            {         //checking condition
-                if(i==1){                //always decr. case
-                    return false;
-                }
-
-                flag=0;
+         int a=0;
+        int temp=1;
+         int flag=0;
+        int n=arr.size();
+        if(n<3)
+        {
+            flag=1;
+        }
+       
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>arr[i-1])
+            {
+               temp++;
             }
-
-//this checks if incr. condn. gets violated (since there is equal to sign)
-            if(flag){
-                if(arr[i]<=arr[i-1]){
-                    return false;
-                }
+            else if(arr[i]==arr[i-1])
+            {
+                flag=1;
+                break;
             }
-            //same as above condition
-            else{
-                if(arr[i]>=arr[i-1]){
-                    return false;
-                }
+            else
+            {
+                break;
             }
         }
-
-        //always increasing condition so returns false as it is not a valid mountain
-        if(flag==1)
-        return false;
-
-/*otherwise return true i.e, after checking all the possible conditions that can make
-an array not a valid mountain it returns true at the end*/
-        return true;
+        if(temp<=1||temp==n)
+        {
+            flag=1;
+        }
+        else
+        {
+            if(flag!=1)
+            {
+        for(int j=temp;j<n-1;j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+             flag=1;
+             break;
+            }
+            else if(arr[j]==arr[j+1])
+            {
+                flag=1;
+                break;
+            }
+        }
+        }
+        }
+      if(flag)
+      return false;
+      else
+      return true; 
     }
 };
