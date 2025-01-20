52b39894fbf04f0dc378959e6dd07ed2f02f254d
mohammad huzaifa <97234805+huzaifa9999@users.noreply.github.com>
2025-01-20 12:37:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factors/prime-factors.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+	vector<int>AllPrimeFactors(int N) {
+	    // Code here
+	    vector<int>ans;
+	    for(int i=2;i<=N;i++)
+	    {
+	        if(N%i==0)
+	        ans.push_back(i);
+	        while(N%i==0)
+	        {
+	            N=N/i;
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		long long int N;
+		cin >> N;
+		Solution ob;
+		vector<int>ans = ob.AllPrimeFactors(N);
+		for(auto i: ans)
+			cout << i <<" ";
+		cout <<"\n";
+	
+cout << "~" << "\n";
+}  
+	return 0;
+}
+// } Driver Code Ends
